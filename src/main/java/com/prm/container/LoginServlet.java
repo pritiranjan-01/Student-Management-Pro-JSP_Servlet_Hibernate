0d3ba00946831dbb16af1f60e19d93cfea738d37
Pritiranjan Mohanty <pritiranjan.mohanty2003@gmail.com>
2025-11-28 18:40:59 +0530

    Updated index.html and web.xml

---
+++ src/main/java/com/prm/container/LoginServlet.java
@@ -0,0 +1,20 @@
+package com.prm.container;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("/WEB-INF/Login.jsp").forward(req, resp);
+    }
+}
